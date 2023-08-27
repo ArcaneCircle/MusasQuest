fe05e433462ffe4a8a80f3ef6a6afb3a69c63aad
Markus Fisch <mf@markusfisch.de>
2023-08-27 19:22:20 +0200

    Add a stage and a speech bubble

--- src/src.js
+++ src/src.js
@@ -1,1 +1,112 @@
 "use strict"
+
+function clear() {
+	clearTimeout(B.tid)
+	B.time = Date.now()
+	B.style.display = "none"
+}
+
+function skip() {
+	if (B.next && Date.now() - B.time > 300) {
+		clear()
+		B.next()
+	}
+}
+
+function say(a, f, cont) {
+	if (!cont && B.talking) {
+		return
+	}
+	clear()
+	const who = a.shift(),
+		what = a.shift()
+	// Set this for getBoundingClientRect() to work as expected.
+	B.style.left = "0px"
+	B.style.top = "0px"
+	B.style.display = "block"
+	if (Array.isArray(what)) {
+		BM.innerText = ""
+		const ol = document.createElement('ol')
+		what.map(function(option) {
+			const text = option.text()
+			if (text) {
+				const li = document.createElement('li'),
+					a = document.createElement('a')
+				a.href = "javascript:void(0)"
+				a.onclick = option.action
+				a.innerHTML = text
+				li.appendChild(a)
+				ol.appendChild(li)
+			}
+		})
+		BM.appendChild(ol)
+		B.next = null
+	} else {
+		BM.innerHTML = what
+		B.talking = (a.length > 0 || f != null) ? 1 : 0
+		B.time = Date.now()
+		B.next = function() {
+			if (a.length > 0) {
+				say(a, f, 1)
+			} else {
+				clear()
+				B.next = null
+				B.talking = 0
+				f && f()
+			}
+		}
+		B.tid = setTimeout(B.next, 1000 + 200 * what.split(' ').length)
+	}
+	const whoRect = who.getBoundingClientRect(),
+		bubbleRect = B.getBoundingClientRect(),
+		margin = parseFloat(getComputedStyle(B).fontSize),
+		whoRectHalfWidth = whoRect.width / 2,
+		ww = window.innerWidth
+	let x = whoRect.x || whoRect.left,
+		cx = x + whoRectHalfWidth
+	if (x + bubbleRect.width >= ww) {
+		x = Math.min(ww - bubbleRect.width - margin,
+			Math.max(margin, x + whoRectHalfWidth - bubbleRect.width / 2))
+	}
+	B.style.left = x + "px"
+	B.style.top = ((whoRect.y || whoRect.top) -
+		bubbleRect.height - margin * 1.5) + "px"
+	BP.style.left = (cx - x) + "px"
+}
+
+function resize() {
+	const windowWidth = window.innerWidth,
+		windowHeight = window.innerHeight,
+		min = Math.min(windowWidth, windowHeight),
+		ratio = min / 300,
+		stageWidth = windowWidth / ratio,
+		stageHeight = windowHeight / ratio
+
+	const style = S.style
+	style.width = stageWidth + "px"
+	style.height = stageHeight + "px"
+	style.transformOrigin = "top left"
+	style.transform = `scale(${ratio})`
+	style.display = "block"
+
+	say([Guy, "Hello World!"])
+}
+
+window.onload = function() {
+	document.onclick = skip
+	document.onkeyup = skip
+	// Prevent pinch/zoom on iOS 11.
+	if ('ontouchstart' in document) {
+		document.addEventListener('gesturestart', function(event) {
+			event.preventDefault()
+		}, false)
+		document.addEventListener('gesturechange', function(event) {
+			event.preventDefault()
+		}, false)
+		document.addEventListener('gestureend', function(event) {
+			event.preventDefault()
+		}, false)
+	}
+	window.onresize = resize
+	resize()
+}
